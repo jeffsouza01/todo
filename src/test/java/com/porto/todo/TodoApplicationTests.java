ce1a0dc5e0f2c72fadf778316fa68edf1d5b2ef4
Jefferson da Silva Souza <jeffersonsilva.souza@portoseguro.com.br>
2024-10-21 22:43:12 -0300

    Update README.MD

--- src/test/java/com/porto/todo/TodoApplicationTests.java
+++ src/test/java/com/porto/todo/TodoApplicationTests.java
@@ -2,15 +2,10 @@ package com.porto.todo;
 
 import com.porto.todo.user.UserModel;
 import com.porto.todo.user.UserRepository;
-import lombok.Getter;
-import lombok.Setter;
-import org.apache.catalina.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.time.LocalDate;
 
 @SpringBootTest
 class TodoApplicationTests {
